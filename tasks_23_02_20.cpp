bd421769ff0345457ecffea923227194a860478d
ALEEZA NADEEM <135522205+Aleezy-nadeem@users.noreply.github.com>
2023-06-05 19:07:19 +0500

    Add files via upload

---
+++ tasks_23_02_20.cpp
@@ -0,0 +1,22 @@
+// Represent a Date in programming using classes and objects
+// in c++ 
+// You should figure our by yourself what are the atrributes
+// data members
+// and functions/member functions/behaviour
+// 23 4 1996
+// get_date
+// Output 23-04-1996
+//Output 23/04/1996
+
+
+
+// Represent a Date in programming using classes and objects
+// in c++ 
+
+// // Represent a Time in programming using classes and objects
+// in c++ 
+
+// Represent a Student in programming using classes and objects
+// in c++ 
+// Represent a Teacher in programming using classes and objects
+// in c++ 
